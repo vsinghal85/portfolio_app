3f15613a31575d42c6f8860c9407654723c79e30
Vaibhav Singhal <vsinghal85@gmail.com>
2017-12-23 11:52:33 -0500

    Added nav bar and added default header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
-
-  
-  def current_user
-  	super ||  OpenStruct.new(name: "Guest User",first_name: "Guest",last_name: "User",email: "guest_user@example.com")
-  end
-
+  include CurrentUserConcern
+  include DefaultPageContent
 end
