a4f8d88655995fb1391bb3f5762594f20c73ebf4
Vaibhav Singhal <vsinghal85@gmail.com>
2018-01-21 02:11:25 -0500

    Implemented initial file uploading system via carrierwave for portfolio items

---
+++ app/uploaders/portfolio_uploader.rb
@@ -0,0 +1,24 @@
+# encoding: utf-8
+
+class PortfolioUploader < CarrierWave::Uploader::Base
+
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
+
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  # def extension_white_list
+  #   %w(jpg jpeg gif png)
+  # end
+
+  
+end
