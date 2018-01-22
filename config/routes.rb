92b48d6c008152ee70a0ff61a7a716c6ac9cb8b6
Vaibhav Singhal <vsinghal85@gmail.com>
2018-01-22 05:49:48 -0500

    Implemented twitter call on tech new page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+   get 'tech-news', to: 'pages#tech_news'
   
 
   resources :blogs do 
